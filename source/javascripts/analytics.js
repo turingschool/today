9c1a9ef16b12ceb0d45821d955e1983c2de49eb4
novohispano <novohispano@gmail.com>
2014-05-14 15:18:34 -0600

    Set up analytics

--- source/javascripts/analytics.js
+++ source/javascripts/analytics.js
@@ -5,5 +5,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-48675805-2', 'turing.io');
+ga('create', 'UA-48675805-3', 'turing.io');
 ga('send', 'pageview');
