cd88cf5c40501fb47bdc6364e4b9b0dbf18732f8
novohispano <novohispano@gmail.com>
2014-05-15 13:57:58 -0600

    Implement post layout for individual blog posts

--- source/2014-05-13-hello-world.html.markdown
+++ source/2014-05-13-hello-world.html.markdown
@@ -4,6 +4,7 @@ title: Hello World
 date: 2014-05-13 20:45 UTC
 author: Jeff Casimir
 tags:
+layout: post
 ---
 
 Fusce a erat ut elit facilisis gravida et sed lorem.
--- source/helpers/view_helpers.rb
+++ source/helpers/view_helpers.rb
@@ -3,6 +3,10 @@ module ViewHelpers
     date.strftime("%b %e, %Y")
   end
 
+  def create_summary(text)
+    text[0..199] << "..."
+  end
+
   def nav_active(page)
     current_page.path == "#{page}.html" ? 'active' : nil
   end
