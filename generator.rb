d296dd12253211ff97da2d8a702b078760a5ffe2
Jeff Casimir <jeff@casimircreative.com>
2014-07-31 22:05:39 -0600

    Generate a simple status page during deploy

--- generator.rb
+++ generator.rb
@@ -1,5 +1,7 @@
 output_file = File.open('./build/build_status.html', 'w')
-output_file << "<h3>Beginning the Build at #{time.now}</h3>"
+output_file.write("<h3>Beginning the Build at #{Time.now}</h3>")
+output_file.close
 system('middleman build')
-output_file << "<h3>Completed the Generator Script at #{time.now}</h3>"
+output_file = File.open('./build/build_status.html', 'w+')
+output_file.write("<h3>Completed the Generator Script at #{Time.now}</h3>")
 output_file.close
