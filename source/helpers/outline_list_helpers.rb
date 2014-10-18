e05e8ee7c44722646082d0f458e3dc5bac6263e2
Josh Cheek <josh.cheek@gmail.com>
2014-10-17 22:07:23 -0600

    /all groups by week
    * Add layout for general content
    * group outlines by week

--- source/helpers/outline_list_helpers.rb
+++ source/helpers/outline_list_helpers.rb
@@ -1,11 +1,31 @@
+require 'date'
+require 'pathname'
+
 module OutlineListHelpers
-  def outline_list(resources)
-    resources.select  { |resource| resource.data.layout == 'outline' }
-             .sort_by { |outline| outline_name(outline.path) }
-             .reverse
+  def outlines_by_week(resources)
+    beginning_of_week = lambda do |date|
+      days_since_monday = date.wday - 1
+      date - days_since_monday
+    end
+
+    # an outline's path takes the form "outlines/yyyy-mm-dd.html"
+    outline_date = lambda do |outline_path|
+      Date.parse(
+        Pathname.new(outline_path)
+                .basename
+                .sub_ext('')
+                .to_s
+      )
+    end
+
+    resources.select   { |resource| resource.data[:layout] == 'outline' }
+             .map      { |outline|  [outline_date.call(outline.path), outline] }
+             .group_by { |date, outline| beginning_of_week.call (date) }
+             .each     { |date, dates_to_outlines| dates_to_outlines.sort_by! &:first }
+             .sort_by  { |date, dates_to_outlines| date }
   end
 
-  def outline_name(path)
-    path.split('/').last.split('.').first
+  def format_date(date)
+    date.strftime '%Y-%m-%d'
   end
 end
