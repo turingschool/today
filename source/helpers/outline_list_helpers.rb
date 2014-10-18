cd50c3e4f97612ad4a7dd5fb1eb5c09f8489cc46
Josh Cheek <josh.cheek@gmail.com>
2014-10-18 00:14:46 -0600

    Uhm, IDK. Something about formatting, the opacity of CSS, finding a vision, and failing to achieve it
    Also, a hypothesis: one reason (among the many) that CSS is so hard to learn is because the names don't make sense.
    e.g. you can position something "absolute", "fixed", "static", which are basically synonyms.
    
    Also, a fact: we need better tooling.

--- source/helpers/outline_list_helpers.rb
+++ source/helpers/outline_list_helpers.rb
@@ -4,6 +4,16 @@ require 'pathname'
 module OutlineListHelpers
   # sooo... this isn't tested.
   # Apparently testing isn't a thing for static sites -.-
+  def outlines_by_month(resources)
+    beginning_of_month = lambda do |date|
+      days_since_first_of_month = date.day - 1
+      date - days_since_first_of_month
+    end
+    outlines_by_week(resources)
+      .group_by { |week_start,  *| beginning_of_month.call week_start }
+      .sort_by  { |month_start, *| month_start }
+  end
+
   def outlines_by_week(resources)
     beginning_of_week = lambda do |date|
       days_since_monday = date.wday - 1
@@ -31,11 +41,19 @@ module OutlineListHelpers
     date.strftime '%Y-%m-%d'
   end
 
+  def yyyy_mm_for(date)
+    date.strftime '%Y-%m'
+  end
+
   def weekday_for(date)
     date.strftime('%A') # e.g. "Wednesday"
   end
 
   def human_date(date)
     date.strftime('%d %B')  # e.g. "15 October"
+  end
+
+  def monthname_for(date)
+    date.strftime('%B')
   end
 end
