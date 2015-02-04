ea6635aaeb609c9410e697dafe7bcd3405b7bbf7
Steve Kinney <hello@stevekinney.net>
2015-02-04 10:36:02 -0700

    Add new cohort

--- source/presenters/outlines_by_cohort.rb
+++ source/presenters/outlines_by_cohort.rb
@@ -4,6 +4,7 @@ require 'time'
 
 class OutlinesByCohort
   COHORTS = [
+    ['1502', Date.parse('2014-02-02')...Date.parse('2015-03-23')],
     ['1412', Date.parse('2014-12-15')...Date.parse('2015-02-02')],
     ['1410', Date.parse('2014-10-27')...Date.parse('2014-12-15')],
     ['1409', Date.parse('2014-09-08')...Date.parse('2014-10-27')],
